30463fdfbba6f631ee6f521b80d0c6bdbb35680b
Nick <20403291+npoduska@users.noreply.github.com>
2024-01-26 17:29:53 -0600

    Add files via upload
    A simple calculator

---
+++ calculator.py
@@ -0,0 +1,70 @@
+logo = """
+ _____________________
+|  _________________  |
+| | Pythonista   0. | |  .----------------.  .----------------.  .----------------.  .----------------. 
+| |_________________| | | .--------------. || .--------------. || .--------------. || .--------------. |
+|  ___ ___ ___   ___  | | |     ______   | || |      __      | || |   _____      | || |     ______   | |
+| | 7 | 8 | 9 | | + | | | |   .' ___  |  | || |     /  \     | || |  |_   _|     | || |   .' ___  |  | |
+| |___|___|___| |___| | | |  / .'   \_|  | || |    / /\ \    | || |    | |       | || |  / .'   \_|  | |
+| | 4 | 5 | 6 | | - | | | |  | |         | || |   / ____ \   | || |    | |   _   | || |  | |         | |
+| |___|___|___| |___| | | |  \ `.___.'\  | || | _/ /    \ \_ | || |   _| |__/ |  | || |  \ `.___.'\  | |
+| | 1 | 2 | 3 | | x | | | |   `._____.'  | || ||____|  |____|| || |  |________|  | || |   `._____.'  | |
+| |___|___|___| |___| | | |              | || |              | || |              | || |              | |
+| | . | 0 | = | | / | | | '--------------' || '--------------' || '--------------' || '--------------' |
+| |___|___|___| |___| |  '----------------'  '----------------'  '----------------'  '----------------' 
+|_____________________|
+"""
+
+
+# Mathmatical functions
+# Add
+def add(n1, n2):
+    return n1+n2
+# Subtract
+def subtract(n1, n2):
+    return n1-n2
+# Multiply
+def multiply(n1, n2):
+    return n1*n2
+# Divide
+def divide(n1, n2):
+    return n1/n2
+
+operations = {
+    "+": add,
+    "-": subtract,
+    "*": multiply,
+    "/": divide
+}
+
+def calculator():
+    print(logo)
+    num1 = float(input("What's the first number?: "))
+
+    for symbol in operations:
+        print(symbol)
+        
+    should_continue = True
+
+    while should_continue:
+        
+        operation_symbol = input("Pick an operation from the line above: ")
+
+        num2 = float(input("What's the second number?: "))
+
+        calculation_function = operations[operation_symbol]
+        answer = calculation_function(num1, num2)
+
+        print(f"{num1} {operation_symbol} {num2} = {answer}")
+        
+        continue_input = input(f"Type 'y' to continue calculating with {answer}, \n or type 'n' to start a new calculation, \n or hit Enter to end program.")
+        if continue_input == "y":
+            num1 = answer
+        elif continue_input =="n":
+            should_continue = False
+            calculator()
+        else:
+            should_continue = False
+            
+
+calculator()
