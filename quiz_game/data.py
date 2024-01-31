834adf9878fd96463794bc68935567f24e09b9f5
Nick <20403291+npoduska@users.noreply.github.com>
2024-01-31 10:52:14 -0600

    Add files via upload

---
+++ quiz_game/data.py
@@ -0,0 +1,114 @@
+#Quiz data from: https://opentdb.com/api_config.php
+
+# question_data = [
+#     {"text": "A slug's blood is green.", "answer": "True"},
+#     {"text": "The loudest animal is the African Elephant.", "answer": "False"},
+#     {
+#         "text": "Approximately one quarter of human bones are in the feet.",
+#         "answer": "True",
+#     },
+#     {
+#         "text": "The total surface area of a human lungs is the size of a football pitch.",
+#         "answer": "True",
+#     },
+#     {
+#         "text": "In West Virginia, USA, if you accidentally hit an animal with your car, you are free to take it home to eat.",
+#         "answer": "True",
+#     },
+#     {
+#         "text": "In London, UK, if you happen to die in the House of Parliament, you are entitled to a state funeral.",
+#         "answer": "False",
+#     },
+#     {"text": "It is illegal to pee in the Ocean in Portugal.", "answer": "True"},
+#     {"text": "You can lead a cow down stairs but not up stairs.", "answer": "False"},
+#     {"text": "Google was originally called 'Backrub'.", "answer": "True"},
+#     {"text": "Buzz Aldrin's mother's maiden name was 'Moon'.", "answer": "True"},
+#     {
+#         "text": "No piece of square dry paper can be folded in half more than 7 times.",
+#         "answer": "False",
+#     },
+#     {"text": "A few ounces of chocolate can to kill a small dog.", "answer": "True"},
+# ]
+
+question_data = [
+    {
+        "type": "boolean",
+        "difficulty": "medium",
+        "category": "General Knowledge",
+        "question": "Coca-Cola&#039;s original colour was green.",
+        "correct_answer": "False",
+        "incorrect_answers": ["True"],
+    },
+    {
+        "type": "boolean",
+        "difficulty": "medium",
+        "category": "Entertainment: Video Games",
+        "question": "In &quot;Resident Evil&quot;, only Chris has access to the grenade launcher.",
+        "correct_answer": "False",
+        "incorrect_answers": ["True"],
+    },
+    {
+        "type": "boolean",
+        "difficulty": "hard",
+        "category": "Politics",
+        "question": "Nazi Germany surrendered on Harry Truman&#039;s birthday while he was president.",
+        "correct_answer": "True",
+        "incorrect_answers": ["False"],
+    },
+    {
+        "type": "boolean",
+        "difficulty": "easy",
+        "category": "History",
+        "question": "Former United States Presidents John Adams and Thomas Jefferson died within hours of each other.",
+        "correct_answer": "True",
+        "incorrect_answers": ["False"],
+    },
+    {
+        "type": "boolean",
+        "difficulty": "medium",
+        "category": "Sports",
+        "question": "Tennis was once known as Racquetball.",
+        "correct_answer": "False",
+        "incorrect_answers": ["True"],
+    },
+    {
+        "type": "boolean",
+        "difficulty": "medium",
+        "category": "Entertainment: Music",
+        "question": "3 members of 2 Live Crew were arrested for playing songs from their album, As Nasty As They Wanna Be, live.",
+        "correct_answer": "True",
+        "incorrect_answers": ["False"],
+    },
+    {
+        "type": "boolean",
+        "difficulty": "medium",
+        "category": "Entertainment: Cartoon &amp; Animations",
+        "question": "In &quot;Avatar: The Last Airbender&quot; and &quot;The Legend of Korra&quot;, Lavabending is a specialized bending technique of Firebending.",
+        "correct_answer": "False",
+        "incorrect_answers": ["True"],
+    },
+    {
+        "type": "boolean",
+        "difficulty": "medium",
+        "category": "Geography",
+        "question": "The capital of the US State Ohio is the city of Chillicothe.",
+        "correct_answer": "False",
+        "incorrect_answers": ["True"],
+    },
+    {
+        "type": "boolean",
+        "difficulty": "hard",
+        "category": "Geography",
+        "question": "The two largest ethnic groups of Belgium are Flemish and Walloon. ",
+        "correct_answer": "True",
+        "incorrect_answers": ["False"],
+    },
+    {
+        "type": "boolean",
+        "difficulty": "easy",
+        "category": "Entertainment: Video Games",
+        "question": "In Kingdom Hearts the Paopu fruit is said to intertwine the destinies for two people forever.",
+        "correct_answer": "True",
+        "incorrect_answers": ["False"],
+    },
+]
